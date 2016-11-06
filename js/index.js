48e2d75ccf4a9e919a7a1c9073d5411637936556
Ortex <ortex@i.ua>
2016-11-06 17:51:38 +0000

    finish

--- js/index.js
+++ js/index.js
@@ -7,15 +7,23 @@ var day = Number(prompt('Day'));
 var sum= add(year, month, day);
 var text = 'Your magic namber: ';
 var result = text + sum;
-document.write(result);
-
-function add(a, b, c) {
-    var result = a + b + c;
-    return result;
+document.write(result);*/
+/*function kvadratne(a, b, c) {
+    var a = Number(prompt('Введіть а яке не дорівнює - 0'));
+    var b = Number(prompt('Введіть - b'));
+    var c = Number(prompt('Введіть - c'));
+    var D = b*b - 4 * a * c;
+    document.write(D);
 }
 */
 
-function tellAge(age) {
+/*function add(a, b, c) {
+    var result = a + b + c;
+    return result;
+}*/
+
+
+/*function tellAge(age) {
     if (age < 18) {
         return 'You are kid';
     } else if(age > 80) {
@@ -23,10 +31,10 @@ function tellAge(age) {
     } else {
         return 'You are adult';
     }
-}
+}*/
 
 /*var age = Number(prompt('Tell your age: '));
 var result = tellAge(age);
 document.write(result);*/
-var result = Math.sqrt(25);
-document.write(result);
+/*var result = Math.sqrt(25);
+document.write(result);*/
